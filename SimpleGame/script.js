49f55e6395c710c4218970e060cec28454903397
KhanhNguyen1102 <sukhanh12@gmail.com>
2021-09-25 21:57:06 +0700

    Simple game

---
+++ SimpleGame/script.js
@@ -0,0 +1,94 @@
+/**
+ * Created by nhatnk on 4/26/17.
+ */
+
+function Hero(image, top, left, size,speed){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+    this.speed = speed;
+    this.firstTop = this.top;
+    this.firstLeft = this.left;
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    }
+    // Di chuyển quanh màn hình = bàn phím
+    this.moveRight = function(){
+        this.left += this.speed;
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        console.log('ok: ' + this.left);
+    }
+    this.moveDown = function(){
+        this.top += this.speed;
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        console.log('ok: ' + this.top);
+    }
+    this.moveLeft = function(){
+        this.left -= this.speed;
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        console.log('ok: ' + this.left);
+    }
+    this.moveUp = function(){
+        this.top -= this.speed;
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        console.log('ok: ' + this.top);
+    }
+    // Auto di chuyển quanh màn hình thì uncomment phần dưới , comment phần trên lại
+    // this.moveRight = function(){
+    //     this.left += this.speed;
+    // }
+    // this.moveDown = function(){
+    //     this.top += this.speed;
+    // }
+    // this.moveLeft = function(){
+    //     this.left -= this.speed;
+    // }
+    // this.moveUp = function(){
+    //     this.top -= this.speed;
+    // }
+
+}
+
+var hero = new Hero('aquarius.jpg', 20, 30, 200, 30);
+
+// Auto di chuyển xung quanh màn hình
+// function start(){
+//     if((hero.left === hero.firstLeft) && (hero.top > hero.firstTop) ){
+//         hero.moveUp();
+//     }else if ((hero.left > window.innerWidth - hero.size) &&(hero.top<window.innerHeight-hero.size)){
+//         hero.moveDown();
+//     } else if ((hero.left < window.innerWidth - hero.size) && (hero.top <= hero.firstTop)){
+//         hero.moveRight();
+//     }else if (hero.top > window.innerHeight - hero.size){
+//         hero.moveLeft();
+//     }
+//     document.getElementById('game').innerHTML = hero.getHeroElement();
+//     setTimeout(start, 500)
+// }
+// start();
+
+// Di chuyển bằng phím
+function moveSelection(key) {
+    switch (key.keyCode) {
+        case 37:
+            hero.moveLeft();
+            break;
+        case 39:
+            hero.moveRight();
+            break;
+        case 38:
+            hero.moveUp();
+            break;
+        case 40:
+            hero.moveDown();
+            break;
+    }
+}
+function doReady() {
+    window.addEventListener('keydown', moveSelection);
+}
+document.getElementById('game').innerHTML = hero.getHeroElement();
