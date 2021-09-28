32f5d5f1113964129657a240f58709af9d66e206
KhanhNguyen1102 <sukhanh12@gmail.com>
2021-09-28 15:55:54 +0700

    Remote Tivi

--- SimpleGame/script.js
+++ SimpleGame/script.js
@@ -15,78 +15,74 @@ function Hero(image, top, left, size,speed){
             ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
     }
     // Di chuyển quanh màn hình = bàn phím
-    this.moveRight = function(){
-        this.left += this.speed;
-        document.getElementById('game').innerHTML = hero.getHeroElement();
-        console.log('ok: ' + this.left);
-    }
-    this.moveDown = function(){
-        this.top += this.speed;
-        document.getElementById('game').innerHTML = hero.getHeroElement();
-        console.log('ok: ' + this.top);
-    }
-    this.moveLeft = function(){
-        this.left -= this.speed;
-        document.getElementById('game').innerHTML = hero.getHeroElement();
-        console.log('ok: ' + this.left);
-    }
-    this.moveUp = function(){
-        this.top -= this.speed;
-        document.getElementById('game').innerHTML = hero.getHeroElement();
-        console.log('ok: ' + this.top);
-    }
-    // Auto di chuyển quanh màn hình thì uncomment phần dưới , comment phần trên lại
     // this.moveRight = function(){
     //     this.left += this.speed;
+    //     document.getElementById('game').innerHTML = hero.getHeroElement();
     // }
     // this.moveDown = function(){
     //     this.top += this.speed;
+    //     document.getElementById('game').innerHTML = hero.getHeroElement();
     // }
     // this.moveLeft = function(){
     //     this.left -= this.speed;
+    //     document.getElementById('game').innerHTML = hero.getHeroElement();
     // }
     // this.moveUp = function(){
     //     this.top -= this.speed;
+    //     document.getElementById('game').innerHTML = hero.getHeroElement();
     // }
+    // Auto di chuyển quanh màn hình thì uncomment phần dưới , comment phần trên lại
+    this.moveRight = function(){
+        this.left += this.speed;
+    }
+    this.moveDown = function(){
+        this.top += this.speed;
+    }
+    this.moveLeft = function(){
+        this.left -= this.speed;
+    }
+    this.moveUp = function(){
+        this.top -= this.speed;
+    }
 
 }
 
-let hero = new Hero('aquarius.jpg', 20, 30, 200, 30);
+let hero = new Hero('aquarius.jpg', 20, 30, 200, 100);
 
 // Auto di chuyển xung quanh màn hình
-// function start(){
-//     if((hero.left === hero.firstLeft) && (hero.top > hero.firstTop) ){
-//         hero.moveUp();
-//     }else if ((hero.left > window.innerWidth - hero.size) &&(hero.top<window.innerHeight-hero.size)){
-//         hero.moveDown();
-//     } else if ((hero.left < window.innerWidth - hero.size) && (hero.top <= hero.firstTop)){
-//         hero.moveRight();
-//     }else if (hero.top > window.innerHeight - hero.size){
-//         hero.moveLeft();
-//     }
-//     document.getElementById('game').innerHTML = hero.getHeroElement();
-//     setTimeout(start, 500)
-// }
-// start();
+function start(){
+    if((hero.left === hero.firstLeft) && (hero.top > hero.firstTop) ){
+        hero.moveUp();
+    }else if ((hero.left > window.innerWidth - hero.size) &&(hero.top<window.innerHeight-hero.size)){
+        hero.moveDown();
+    } else if ((hero.left < window.innerWidth - hero.size) && (hero.top <= hero.firstTop)){
+        hero.moveRight();
+    }else if (hero.top > window.innerHeight - hero.size){
+        hero.moveLeft();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 500)
+}
+start();
 
 // Di chuyển bằng phím
-function moveSelection(key) {
-    switch (key.keyCode) {
-        case 37:
-            hero.moveLeft();
-            break;
-        case 39:
-            hero.moveRight();
-            break;
-        case 38:
-            hero.moveUp();
-            break;
-        case 40:
-            hero.moveDown();
-            break;
-    }
-}
-function doReady() {
-    window.addEventListener('keydown', moveSelection);
-}
-document.getElementById('game').innerHTML = hero.getHeroElement();
+// function moveSelection(key) {
+//     switch (key.keyCode) {
+//         case 37:
+//             hero.moveLeft();
+//             break;
+//         case 39:
+//             hero.moveRight();
+//             break;
+//         case 38:
+//             hero.moveUp();
+//             break;
+//         case 40:
+//             hero.moveDown();
+//             break;
+//     }
+// }
+// function doReady() {
+//     window.addEventListener('keydown', moveSelection);
+// }
+// document.getElementById('game').innerHTML = hero.getHeroElement();
